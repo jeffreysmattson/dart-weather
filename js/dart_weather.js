af0b4fba9ae1102a3b3337b219dd627ee1b7cfa1
Jeff Mattson <jeff@layer7web.com>
2017-11-11 16:51:30 -0500

    Removing all characters except numbers from input.

--- js/dart_weather.js
+++ js/dart_weather.js
@@ -37,6 +37,10 @@
      * @return string on error
      */
     getWeatherData : function(zip = '03755'){
+
+      /* No funny business */
+      zip = zip.replace(/\D/g,'');
+      
       var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27'+zip+'%27)&format=json';
       jQuery.ajax({
         dataType: "json",
