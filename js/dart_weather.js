414ee0bb1cadc552027c22001762f2473cd9d785
Jeff Mattson <jeff@layer7web.com>
2017-11-11 17:13:31 -0500

    Removed console.log

--- js/dart_weather.js
+++ js/dart_weather.js
@@ -42,7 +42,7 @@
       zip = zip.replace(/\D/g,'');
       
       var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27'+zip+'%27)&format=json';
-      jQuery.ajax({
+      $.ajax({
         dataType: "json",
         url: url,
         success: function(data) {
@@ -55,15 +55,14 @@
             return;
           } else {
             weather_js.clearWeatherHtml();
-            jQuery('#js_weatherResultsTitle').html('Sorry, no results for that ZIP code.');
+            $('#js_weatherResultsTitle').html('Sorry, no results for that ZIP code.');
           }
         },
         error: function(jqXHR, textStatus, errorThrown) {
           weather_js.clearWeatherHtml();
-          jQuery('#js_weatherResultsTitle').html('Sorry, no results for that ZIP code.');
+          $('#js_weatherResultsTitle').html('Sorry, no results for that ZIP code.');
         },
         complete: function(jqXHR, textStatus){
-          console.log(jqXHR);
           if(textStatus == 'success' && weather_js.results != null){
             weather_js.insertWeatherHtml();
           }
@@ -78,14 +77,14 @@
      */
     addListeners : function(){
       $('#getWeatherButton').on('click', function(e){
-        var zipCode = jQuery('#zipCode').val();
+        var zipCode = $('#zipCode').val();
         weather_js.getWeatherData(zipCode);
       });
 
       /* Submit on return key as well */
       $('input#zipCode').keypress(function (e) {
         if (e.which == 13) {
-          var zipCode = jQuery('#zipCode').val();
+          var zipCode = $('#zipCode').val();
           weather_js.getWeatherData(zipCode);
         }
        });
@@ -97,10 +96,10 @@
      * @return void
      */
     insertWeatherHtml : function(){
-      jQuery('#js_weatherResultsTitle').html(weather_js.title);
-      jQuery('#js_weatherResultsTemp').html(weather_js.temp + ' ' + weather_js.scale);
-      jQuery('#js_weatherResultsText').html(weather_js.text);
-      jQuery('#js_weatherResultsLink').html('<a href="'+weather_js.more+'" target="_blank">more</a>');
+      $('#js_weatherResultsTitle').html(weather_js.title);
+      $('#js_weatherResultsTemp').html(weather_js.temp + ' ' + weather_js.scale);
+      $('#js_weatherResultsText').html(weather_js.text);
+      $('#js_weatherResultsLink').html('<a href="'+weather_js.more+'" target="_blank">more</a>');
     },
 
     /*
@@ -109,10 +108,10 @@
      * @return void
      */
     clearWeatherHtml : function(){
-      jQuery('#js_weatherResultsTitle').html('');
-      jQuery('#js_weatherResultsTemp').html('');
-      jQuery('#js_weatherResultsText').html('');
-      jQuery('#js_weatherResultsLink').html('');
+      $('#js_weatherResultsTitle').html('');
+      $('#js_weatherResultsTemp').html('');
+      $('#js_weatherResultsText').html('');
+      $('#js_weatherResultsLink').html('');
     }
   }
 })(jQuery);
